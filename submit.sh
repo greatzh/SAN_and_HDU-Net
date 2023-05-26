cb387b8ae5e364d1635e9330bcef12e985614ed8
greatzh <zh.zhang@connect.um.edu.mo>
2023-05-26 19:52:53 +0800

    test again

--- submit.sh
+++ submit.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name                     FSPNet
+#SBATCH --job-name                     san
 #SBATCH --partition                    gpu-normal
 #SBATCH --nodes                        1
 #SBATCH --tasks-per-node               6
@@ -8,8 +8,8 @@
 #SBATCH --mem                             40G
 #SBATCH --gres                          gpu:1
 #SBATCH --constraint                    GTX1080
-#SBATCH --output                        FSPNet.%j.out
-#SBATCH --error                         FSPNet.%j.err
+#SBATCH --output                        san.%j.out
+#SBATCH --error                         san.%j.err
 #SBATCH --mail-type		ALL
 #SBATCH --mail-user		zh.zhang@connect.um.edu.mo
  
@@ -22,4 +22,6 @@ module add cuda/10.0.130
 ulimit -s unlimited
  
 # singularity exec --nv --bind /data:/data /share/apps/singularity/simg/pytorch/miniconda3-pytorch bash -c "source activate py36; which python;pip list;python -c 'import torch as t; print(t.cuda.is_available(),t.cuda.device_count())'"
-singularity exec --nv --bind /data:/data /share/apps/singularity/simg/pytorch/miniconda3-pytorch bash -c "bash train_SAN.sh"
+# singularity exec --nv --bind /data:/data /share/apps/singularity/simg/pytorch/miniconda3-pytorch bash -c "bash train_SAN.sh"
+echo $CUDA_VISIBLE_DEVICES
+srun bash train_SAN.sh
